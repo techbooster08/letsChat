e73e551425f250660ab43f24d20cdd54efcc9ff1
techbooster08 <akashpawar.dev.gmail.com>
2026-01-11 18:41:04 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import apiV1Routes from "./routes/apiV1.route.js";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use("/api/v1", apiV1Routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
