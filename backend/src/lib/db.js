048c5dabc919651959cfc7181af85e80b2e4c758
techbooster08 <akashpawar.dev.gmail.com>
2026-01-24 12:38:20 +0530

    added welcome email service

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import moongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const { MONGO_URI } = process.env;
+    const MONGO_URI = ENV.MONGO_URI;
     if (!MONGO_URI) {
       throw new Error("MONGO_URI is not defined in environment variables");
     }
